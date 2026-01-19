190b04f8e98b4bf4fbda7f561270df20ca522a13
Kyle Hsiao <kylehsiao@google.com>
2026-01-19 02:08:44 -0800

    nfc: Modify Secure NFC restoration to use the corresponding userId
    Bug: 368376056
    Bug: 475315815
    Bug: 475447365
    Test: gts-tradefed run gts -m GtsBackupTestCases -t com.google.android.gts.backup.NfcBackupTest
    Flag: EXEMPT BUGFIX
    (cherry picked from commit bffc57339cf2105bb0a12cb754f7b4c4e09ddb45)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:4e202097e629c668889376bab367763940fd3529
    Merged-In: I94153797e71acd3951a5f3cce002e6a508b64c3f
    Change-Id: I94153797e71acd3951a5f3cce002e6a508b64c3f

--- src/com/android/nfc/NfcBackupAgent.java
+++ src/com/android/nfc/NfcBackupAgent.java
@@ -16,6 +16,7 @@
 
 package com.android.nfc;
 
+import android.app.ActivityManager;
 import android.app.backup.BackupAgentHelper;
 import android.app.backup.SharedPreferencesBackupHelper;
 import android.content.Context;
@@ -38,6 +39,7 @@ public class NfcBackupAgent extends BackupAgentHelper {
     public void onRestoreFinished() {
         NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         NfcService.sIsNfcRestore = true;
+        DeviceConfigFacade deviceConfigFacade = NfcInjector.getInstance().getDeviceConfigFacade();
 
         if (nfcAdapter != null) {
             SharedPreferences prefs = getSharedPreferences(NfcService.PREF,
@@ -56,7 +58,9 @@ public class NfcBackupAgent extends BackupAgentHelper {
                 nfcAdapter.enableReaderOption(false);
             }
 
-            if (prefs.getBoolean(NfcService.PREF_SECURE_NFC_ON, NfcService.SECURE_NFC_ON_DEFAULT)
+            int userId = ActivityManager.getCurrentUser();
+            if (prefs.getBoolean(NfcService.PREF_SECURE_NFC_ON + "_" + userId,
+                    deviceConfigFacade.getDefaultSecureNfcState())
                     && nfcAdapter.isSecureNfcSupported()) {
                 nfcAdapter.enableSecureNfc(true);
             } else {
